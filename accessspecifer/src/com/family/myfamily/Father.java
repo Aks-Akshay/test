97c0ed10f52264822ba068518d27069245d02e4c
aksha <aksha@Akshay>
2022-11-09 17:11:15 +0530

    new program

---
+++ accessspecifer/src/com/family/myfamily/Father.java
@@ -0,0 +1,20 @@
+package com.family.myfamily;
+
+public class Father {
+private void atm()
+{
+	System.out.println("dad kaa atm");
+}
+void car()
+{
+	System.out.println("dad car");
+}
+public void bike()
+{
+	System.out.println("dad ka bike");
+}
+public void cycle()
+{
+	System.out.println("dad ka cycle");
+}
+}
